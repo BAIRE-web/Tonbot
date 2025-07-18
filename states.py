6439c42b5b72fc061c7f00809015c0a07d4b3ec7
BAIRE-web <barakaeduc.ia@gmail.com>
2025-07-18 20:32:21 +0000

    mise à jour du scipt et ajout des json

--- states.py
+++
@@ -1,8 +0,0 @@
-class DéfiStates(StatesGroup):
-    choix_niveau = State()
-    choix_type = State()
-    choix_matiere = State()
-    choix_chapitre = State()
-    concours_type = State()
-    attente_joueur2 = State()
-    defis_en_cours = State()
